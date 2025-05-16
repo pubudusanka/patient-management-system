0575d7fadf325701a1a387a9de260eea238314bb
Pubudu Sanka <pubudusanka79@gmail.com>
2025-05-16 16:16:25 +0530

    prettier the code

--- patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -9,6 +9,7 @@ import java.time.LocalDate;
 public class PatientMapper {
     public static PatientResponseDTO toDTO(Patient patient) {
         PatientResponseDTO patientDTO = new PatientResponseDTO();
+
         patientDTO.setId(patient.getId().toString());
         patientDTO.setName(patient.getName());
         patientDTO.setAddress(patient.getAddress());
@@ -20,11 +21,13 @@ public class PatientMapper {
 
     public static Patient toModel(PatientRequestDTO patientRequestDTO) {
         Patient patient = new Patient();
+
         patient.setName(patientRequestDTO.getName());
         patient.setAddress(patientRequestDTO.getAddress());
         patient.setEmail(patientRequestDTO.getEmail());
         patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
         patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+
         return patient;
     }
 }
